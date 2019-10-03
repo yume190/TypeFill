e5edf3cd34eda7102aea928fc4dd5ab1714a3ab2
煒峻林 <yume190@gmail.com>
2019-10-03 16:49:00 +0800

    add some sample to test AttributeRewriter

--- example/sample.swift
+++ example/sample.swift
@@ -1,1 +1,15 @@
-let a = 1
+import UIKit
+
+let a: Int = 1
+
+class A: UIViewController {
+    @IBOutlet final open weak var btn1: UIButton!
+    @IBOutlet final public weak var btn2: UIButton!
+    @IBOutlet final internal weak var btn3: UIButton!
+    @IBOutlet final fileprivate weak var btn4: UIButton!
+    @IBOutlet final private weak var btn5: UIButton!
+    @IBOutlet private final var btn6: UIButton!
+    @IBAction private final func numberKeyIn(_ sender: UIButton) {
+    }
+}
+
