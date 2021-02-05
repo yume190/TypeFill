86a5efb4977834989e75a54c8a9be2a7cd34989d
Yume <yume190@gmail.com>
2021-02-05 17:17:41 +0800

    fix spm command arguments

--- Sources/TypeFillKit/Command/SPMModule.swift
+++ Sources/TypeFillKit/Command/SPMModule.swift
@@ -27,19 +27,19 @@ struct SPMModule: ParsableCommand, CommandBase {
     @Flag(name: [.customLong("print", withSingleDash: false)], help: "print fixed code")
     var print: Bool = false
     
-    @Argument(help: "Arguments passed to `xcodebuild` or `swift build`. If `-` prefixed argument exists, place ` -- ` before that.")
-    var args: [String]
-    
-    @Argument(help: "")
+    @Option(name: [.customLong("moduleName", withSingleDash: false)], help: "spm target name")
     var moduleName: String
     
+    @Argument(help: "Arguments passed to `xcodebuild` or `swift build`. If `-` prefixed argument exists, place ` -- ` before that.")
+    var args: [String] = []
+    
     func run() throws {
-        let module = Module(spmArguments: args, spmName: moduleName)
+        let module: Module? = Module(spmArguments: args, spmName: moduleName)
 //        guard let docs = Module(spmArguments: args, spmName: moduleName)?.docs else {
 //            throw SourceKittenError.docFailed
 //        }
 //        try self.rewrite(rewriter: rewriter, docsList: docs)
-        try module?.sourceFiles.forEach{ (filePath) in
+        try module?.sourceFiles.forEach{ (filePath: String) in
             guard let file = File(path: filePath) else {return}
             let url = URL(fileURLWithPath: filePath)
             let cursor: Cursor = .init(filePath: filePath, arguments: module?.compilerArguments ?? [])
