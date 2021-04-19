bfb1dd442e3b137aabf5b136e645eef8fef3c273
Yume <yume190@gmail.com>
2021-04-19 10:14:58 +0800

    change test file's name

--- Tests/TypeFillTests/AutoFillTests.swift
+++ Tests/TypeFillTests/AutoFillTests.swift
@@ -29,7 +29,7 @@ final class AutoFillTests: XCTestCase {
     /// let a = 1
     /// var b = a
     final func testType() throws {
-        let file = resource(file: "Decl.swift")
+        let file = resource(file: "Decl.swift.data")
         let args = [file, "-sdk", sdkPath()]
         let cursor = Cursor(filePath: file, arguments: args)
         let type = try cursor(4)
@@ -39,7 +39,7 @@ final class AutoFillTests: XCTestCase {
     /// let a = 1
     /// var b = a
     final func testDecl() throws {
-        let override = try rewriter(file: "Decl.swift").dump()
+        let override = try rewriter(file: "Decl.swift.data").dump()
         let result = """
         let a: Int = 1
         var b: Int = a
@@ -49,9 +49,38 @@ final class AutoFillTests: XCTestCase {
     
     /// let (a, b) = (1, 2)
     final func testTuple() throws {
-        let override = try rewriter(file: "Tuple.swift").dump()
+        let override = try rewriter(file: "Tuple.swift.data").dump()
         let result = """
         let (a, b): (Int, Int) = (1, 2)
+        """
+        XCTAssertEqual(override, result)
+    }
+    
+    
+    /// struct Test {
+    ///     let a, b: Int
+    ///     let c = 1, d = 2
+    /// }
+    final func testDeclWithComma() throws {
+        let override = try rewriter(file: "DeclWithComma.swift.data").dump()
+        let result = """
+        struct Test {
+            let a: Int, b: Int
+            let c: Int = 1, d: Int = 2
+        }
+        """
+        XCTAssertEqual(override, result)
+    }
+    
+    /// typealias NewInt = Int
+    /// let a: NewInt = 1
+    /// let b = a
+    final func testTypeAlias() throws {
+        let override = try rewriter(file: "TypeAlias.swift.data").dump()
+        let result = """
+        typealias NewInt = Int
+        let a: NewInt = 1
+        let b: NewInt = a
         """
         XCTAssertEqual(override, result)
     }
@@ -60,7 +89,7 @@ final class AutoFillTests: XCTestCase {
     ///     return ""
     /// }
     final func testClosure1() throws {
-        let override = try rewriter(file: "Closure1.swift").dump()
+        let override = try rewriter(file: "Closure1.swift.data").dump()
         let result = """
         let a: (Int, Int) -> String = { (a: Int, b: Int) -> String in
             return ""
@@ -73,7 +102,7 @@ final class AutoFillTests: XCTestCase {
     ///     return ""
     /// }
     final func testClosure2() throws {
-        let override = try rewriter(file: "Closure2.swift").dump()
+        let override = try rewriter(file: "Closure2.swift.data").dump()
         let result = """
         let a: (Int, Int) -> String = { (a: Int, b: Int) -> String in
             return ""
@@ -86,7 +115,7 @@ final class AutoFillTests: XCTestCase {
     ///     return
     /// }
     final func testClosureEmpty() throws {
-        let override = try rewriter(file: "ClosureEmpty.swift").dump()
+        let override = try rewriter(file: "ClosureEmpty.swift.data").dump()
         let result = """
         let a: () -> Void = {
             return
@@ -98,7 +127,7 @@ final class AutoFillTests: XCTestCase {
     /// let a: Int? = nil
     /// if let aa = a {}
     final func testIf() throws {
-        let override = try rewriter(file: "If.swift").dump()
+        let override = try rewriter(file: "If.swift.data").dump()
         let result = """
         let a: Int? = nil
         if let aa: Int = a {}
@@ -109,7 +138,7 @@ final class AutoFillTests: XCTestCase {
     /// let a: Int? = nil
     /// guard let aa = a else {return}
     final func testGuard() throws {
-        let override = try rewriter(file: "Guard.swift").dump()
+        let override = try rewriter(file: "Guard.swift.data").dump()
         let result = """
         let a: Int? = nil
         guard let aa: Int = a else {return}
@@ -125,12 +154,12 @@ final class AutoFillTests: XCTestCase {
     }
     // MARK: skip inout
     final func testInout() throws {
-        let override = try rewriter(file: "Inout.swift").dump()
-        let trueResult = """
-        let a: (inout Int) -> Int = { (i: inout Int) in
-            return i
-        }
-        """
+        let override = try rewriter(file: "Inout.swift.data").dump()
+        /// let trueResult = """
+        /// let a: (inout Int) -> Int = { (i: inout Int) in
+        ///     return i
+        /// }
+        /// """
         let result = """
         let a: (inout Int) -> Int = { i in
             return i
