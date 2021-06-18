042329eeb5d0fd0a3564eac6bcc7bb6f11e738b8
Yume <yume190@gmail.com>
2021-06-18 11:57:53 +0800

    Update to 0.1.9

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION = 0.1.8
+VERSION = 0.1.9
 
 include SourceKitten.mk
 include TypeFill.mk
--- Sources/TypeFill/Command/TypeFill.swift
+++ Sources/TypeFill/Command/TypeFill.swift
@@ -19,7 +19,7 @@ public struct TypeFill: ParsableCommand {
     public static var configuration: CommandConfiguration = CommandConfiguration(
         commandName: "typefill",
         abstract: "A utility for fill swift types.",
-        version: "0.1.8",
+        version: "0.1.9",
         subcommands: [SingleFile.self, SPMModule.self, WorkSpace.self, Project.self],
         defaultSubcommand: SPMModule.self
     )
