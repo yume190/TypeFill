b98d1065b6f0c345974511c77e6e442ca0234e03
Yume <yume190@gmail.com>
2021-10-26 11:58:20 +0800

    Update to 0.2.3

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION = 0.2.2
+VERSION = 0.2.3
 
 include SourceKitten.mk
 include TypeFill.mk
--- Sources/LeakDetect/main.swift
+++ Sources/LeakDetect/main.swift
@@ -24,7 +24,7 @@ struct Command: ParsableCommand {
          * assign: detect assign instance function `x = self.func` or `y(self.func)`.
          * capture: detect `self` capture in closure.
         """,
-        version: "0.2.2"
+        version: "0.2.3"
     )
     
     @Flag(name: [.customLong("verbose", withSingleDash: false), .short], help: "print inpect time")
