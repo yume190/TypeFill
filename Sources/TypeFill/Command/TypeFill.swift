9ce4612113898116015d5a32a26a7660358f33b4
Yume <yume190@gmail.com>
2021-12-24 16:27:57 +0800

    Update to 0.2.5.4

--- Sources/TypeFill/Command/TypeFill.swift
+++ Sources/TypeFill/Command/TypeFill.swift
@@ -19,7 +19,7 @@ public struct TypeFill: ParsableCommand {
     public static var configuration: CommandConfiguration = CommandConfiguration(
         commandName: "typefill",
         abstract: "A utility for fill swift types.",
-        version: "0.2.5.3",
+        version: "0.2.5.4",
         subcommands: [XCode.self, SingleFile.self, SPMModule.self, WorkSpace.self, Project.self],
         defaultSubcommand: XCode.self
     )
