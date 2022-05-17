42f94ba4b04965f21d88a324dd8753d66e1e5d31
Yume <yume190@gmail.com>
2022-05-17 13:16:16 +0800

    use `@_exported` to reuse SwiftSyntax

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@
 import PackageDescription
 import Foundation
 
-#if swift(>=5.5)
-let branch = "release/5.5"
-#elseif swift(>=5.4)
-let branch = "release/5.4"
-#else
-let branch = "release/5.3"
-#endif
-
-let appleDependencies: [Package.Dependency] = [
-    // .package(name: "IndexStoreDB", url: "https://github.com/apple/indexstore-db.git", .branch(branch)),
-    .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .branch(branch)),
-]
+//#if swift(>=5.5)
+//let branch = "release/5.5"
+//#elseif swift(>=5.4)
+//let branch = "release/5.4"
+//#else
+//let branch = "release/5.3"
+//#endif
+//
+//let appleDependencies: [Package.Dependency] = [
+//    // .package(name: "IndexStoreDB", url: "https://github.com/apple/indexstore-db.git", .branch(branch)),
+//    .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .branch(branch)),
+//]
 
 let package = Package(
     name: "TypeFill",
@@ -31,12 +31,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
+        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50600.1")),
+        
         .package(url: "https://github.com/jpsim/SourceKitten", .upToNextMinor(from: "0.31.1")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "1.0.3")),
         
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift", .upToNextMinor(from: "1.4.3")),
         .package(url: "https://github.com/onevcat/Rainbow", from: "4.0.1"),
-    ] + appleDependencies,
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
@@ -76,27 +78,32 @@ let package = Package(
             name: "TypeFillKit",
             dependencies: [
                 "Rainbow",
-                .product(name: "SwiftSyntax", package: "SwiftSyntax"),
+                "_SwiftSyntax",
                 // .product(name: "IndexStoreDB", package: "IndexStoreDB"),
                 "Cursor",
-            ]),
+            ]
+        ),
         
         .target(
             name: "SwiftLeakCheck",
             dependencies: [
                 "Rainbow",
-                .product(name: "SwiftSyntax", package: "SwiftSyntax"),
+                "_SwiftSyntax",
+                "_SwiftSyntaxParser",
                 "Cursor",
                 "LeakDetectExtension",
-            ]),
+            ]
+        ),
         
         .target(
             name: "LeakDetectExtension",
             dependencies: [
                 "Rainbow",
-                .product(name: "SwiftSyntax", package: "SwiftSyntax"),
+                "_SwiftSyntax",
+                "_SwiftSyntaxParser",
                 "Cursor",
-            ]),
+            ]
+        ),
         
         // MARK: Common Frameworks
         .target(
@@ -110,15 +117,17 @@ let package = Package(
             dependencies: [
                 "Rainbow",
                 .product(name: "SourceKittenFramework", package: "SourceKitten"),
-                .product(name: "SwiftSyntax", package: "SwiftSyntax"),
+                "_SwiftSyntax",
+                "_SwiftSyntaxParser",
                 "Derived",
-            ]),
+            ]
+        ),
         
         // MARK: Tests
         .testTarget(
             name: "TypeFillTests",
             dependencies: [
-                .product(name: "SwiftSyntax", package: "SwiftSyntax"),
+                "_SwiftSyntax",
                 "TypeFillKit",
                 "Cursor",
             ],
@@ -130,7 +139,7 @@ let package = Package(
         .testTarget(
             name: "LeakDetectTests",
             dependencies: [
-                .product(name: "SwiftSyntax", package: "SwiftSyntax"),
+                "_SwiftSyntax",
                 "LeakDetectExtension",
                 "SwiftLeakCheck",
                 "Cursor",
@@ -149,5 +158,40 @@ let package = Package(
                 .copy("Resource")
             ]
         ),
+    
+        // MARK: Swift Syntax
+        // from https://github.com/krzysztofzablocki/Sourcery/blob/master/Package.swift
+        // Pass `-dead_strip_dylibs` to ignore the dynamic version of `lib_InternalSwiftSyntaxParser`
+        // that ships with SwiftSyntax because we want the static version from
+        // `StaticInternalSwiftSyntaxParser`.
+        .target(
+            name: "_SwiftSyntax",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "SwiftSyntax"),
+                "lib_InternalSwiftSyntaxParser",
+                
+            ],
+            linkerSettings: [
+                .unsafeFlags(["-Xlinker", "-dead_strip_dylibs"])
+            ]
+        ),
+        
+        .target(
+            name: "_SwiftSyntaxParser",
+            dependencies: [
+                .product(name: "SwiftSyntaxParser", package: "SwiftSyntax"),
+                "lib_InternalSwiftSyntaxParser",
+                
+            ],
+            linkerSettings: [
+                .unsafeFlags(["-Xlinker", "-dead_strip_dylibs"])
+            ]
+        ),
+        
+        .binaryTarget(
+            name: "lib_InternalSwiftSyntaxParser",
+            url: "https://github.com/keith/StaticInternalSwiftSyntaxParser/releases/download/5.6/lib_InternalSwiftSyntaxParser.xcframework.zip",
+            checksum: "88d748f76ec45880a8250438bd68e5d6ba716c8042f520998a438db87083ae9d"
+        )
     ]
 )
---
+++ Sources/_SwiftSyntax/Export.swift
@@ -0,0 +1,8 @@
+//
+//  Export.swift
+//  
+//
+//  Created by Yume on 2022/5/17.
+//
+
+@_exported import SwiftSyntax
