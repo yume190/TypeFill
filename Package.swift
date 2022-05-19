beb8fb92bab83ebc3382ae65248375bebe22ce39
Yume <yume190@gmail.com>
2022-05-19 15:54:32 +0800

    fix package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,7 +11,51 @@ import Foundation
 //#else
 //let branch = "release/5.3"
 //#endif
-//
+
+let isLibrary = true
+
+// from https://github.com/krzysztofzablocki/Sourcery/blob/master/Package.swift
+// Pass `-dead_strip_dylibs` to ignore the dynamic version of `lib_InternalSwiftSyntaxParser`
+// that ships with SwiftSyntax because we want the static version from
+// `StaticInternalSwiftSyntaxParser`.
+let _targets: [Target] = [
+    .target(
+        name: "_SwiftSyntax",
+        dependencies: [
+            .product(name: "SwiftSyntax", package: "SwiftSyntax"),
+            "lib_InternalSwiftSyntaxParser",
+            
+        ],
+        linkerSettings: [
+            .unsafeFlags(["-Xlinker", "-dead_strip_dylibs"])
+        ]
+    ),
+
+    .target(
+        name: "_SwiftSyntaxParser",
+        dependencies: [
+            .product(name: "SwiftSyntaxParser", package: "SwiftSyntax"),
+            "lib_InternalSwiftSyntaxParser",
+            
+        ],
+        linkerSettings: [
+            .unsafeFlags(["-Xlinker", "-dead_strip_dylibs"])
+        ]
+    ),
+
+    .binaryTarget(
+        name: "lib_InternalSwiftSyntaxParser",
+        url: "https://github.com/keith/StaticInternalSwiftSyntaxParser/releases/download/5.6/lib_InternalSwiftSyntaxParser.xcframework.zip",
+        checksum: "88d748f76ec45880a8250438bd68e5d6ba716c8042f520998a438db87083ae9d"
+    ),
+]
+let targets = isLibrary ?  [] : _targets
+let swiftSyntax: Target.Dependency = isLibrary ?
+    .product(name: "SwiftSyntax", package: "SwiftSyntax") : "_SwiftSyntax"
+let swiftSyntaxParser: Target.Dependency = isLibrary ?
+    .product(name: "SwiftSyntaxParser", package: "SwiftSyntax") : "_SwiftSyntaxParser"
+
+
 //let appleDependencies: [Package.Dependency] = [
 //    // .package(name: "IndexStoreDB", url: "https://github.com/apple/indexstore-db.git", .branch(branch)),
 //    .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .branch(branch)),
@@ -29,17 +73,15 @@ let package = Package(
         
         .library(name: "TypeFillKit", targets: ["TypeFillKit"]),
         .library(name: "Cursor", targets: ["Cursor"]),
-        .library(name: "StaticSwiftSyntax", targets: ["_SwiftSyntax"]),
-        .library(name: "StaticSwiftSyntaxParser", targets: ["_SwiftSyntaxParser"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50600.1")),
         
-        .package(url: "https://github.com/jpsim/SourceKitten", .upToNextMinor(from: "0.31.1")),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "1.0.3")),
+        .package(url: "https://github.com/jpsim/SourceKitten", from: "0.32.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.1.0"),
         
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", .upToNextMinor(from: "1.4.3")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.5.0"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "4.0.1"),
     ],
     targets: [
@@ -47,7 +89,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         
         // MARK: Executable
-        .target(
+        .executableTarget(
             name: "TypeFill",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
@@ -57,7 +99,7 @@ let package = Package(
             ]
         ),
 
-        .target(
+        .executableTarget(
             name: "DerivedPath",
             dependencies: [
                 "Derived"
@@ -80,8 +122,8 @@ let package = Package(
         .target(
             name: "TypeFillKit",
             dependencies: [
+                swiftSyntax,
                 "Rainbow",
-                "_SwiftSyntax",
                 // .product(name: "IndexStoreDB", package: "IndexStoreDB"),
                 "Cursor",
             ]
@@ -118,10 +160,10 @@ let package = Package(
         .target(
             name: "Cursor",
             dependencies: [
+                swiftSyntax,
+                swiftSyntaxParser,
                 "Rainbow",
                 .product(name: "SourceKittenFramework", package: "SourceKitten"),
-                "_SwiftSyntax",
-                "_SwiftSyntaxParser",
                 "Derived",
             ]
         ),
@@ -130,7 +172,7 @@ let package = Package(
         .testTarget(
             name: "TypeFillTests",
             dependencies: [
-                "_SwiftSyntax",
+                swiftSyntax,
                 "TypeFillKit",
                 "Cursor",
             ],
@@ -161,40 +203,5 @@ let package = Package(
                 .copy("Resource")
             ]
         ),
-    
-        // MARK: Swift Syntax
-        // from https://github.com/krzysztofzablocki/Sourcery/blob/master/Package.swift
-        // Pass `-dead_strip_dylibs` to ignore the dynamic version of `lib_InternalSwiftSyntaxParser`
-        // that ships with SwiftSyntax because we want the static version from
-        // `StaticInternalSwiftSyntaxParser`.
-        .target(
-            name: "_SwiftSyntax",
-            dependencies: [
-                .product(name: "SwiftSyntax", package: "SwiftSyntax"),
-                "lib_InternalSwiftSyntaxParser",
-                
-            ],
-            linkerSettings: [
-                .unsafeFlags(["-Xlinker", "-dead_strip_dylibs"])
-            ]
-        ),
-        
-        .target(
-            name: "_SwiftSyntaxParser",
-            dependencies: [
-                .product(name: "SwiftSyntaxParser", package: "SwiftSyntax"),
-                "lib_InternalSwiftSyntaxParser",
-                
-            ],
-            linkerSettings: [
-                .unsafeFlags(["-Xlinker", "-dead_strip_dylibs"])
-            ]
-        ),
-        
-        .binaryTarget(
-            name: "lib_InternalSwiftSyntaxParser",
-            url: "https://github.com/keith/StaticInternalSwiftSyntaxParser/releases/download/5.6/lib_InternalSwiftSyntaxParser.xcframework.zip",
-            checksum: "88d748f76ec45880a8250438bd68e5d6ba716c8042f520998a438db87083ae9d"
-        )
-    ]
+    ] + targets
 )
